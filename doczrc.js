e2f67116231840543dc22e385f5c7184a677fa03
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-07-15 11:06:31 +0900

    Add remark plugins

--- doczrc.js
+++ doczrc.js
@@ -1,4 +1,7 @@
 import path from 'path';
+import { default as externalLinks } from 'remark-external-links';
+import { default as images } from 'remark-images';
+import { default as emoji } from 'remark-emoji';
 
 const PUBLIC = path.resolve(__dirname, 'public');
 const DOCS = path.resolve(__dirname, 'docs');
@@ -21,11 +24,17 @@ export default {
   theme: 'src/theme',
   themeConfig: {
     styles: {
+      body: {
+        fontFamily: "'rotobo', Helvetica, sans-serif",
+        fontSize: '16px',
+        lineHeight: 1.6
+      },
       pre: {
         background: '#13161F',
         color: '#efefef'
       }
     }
   },
+  mdPlugins: [externalLinks, images, emoji],
   modifyBundlerConfig
 };
